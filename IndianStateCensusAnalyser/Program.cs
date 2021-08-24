116cfb3b7a367ef8bb82bd24d89e2330f7f99362
dnyana <dnyaneshgawade903@gmail.com>
2021-08-24 20:29:33 +0530

    [Dnyanesh] Add : Wellcome message for Indian State Census Analyser

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to the Indian State Census Analyser!");
+        }
+    }
+}
